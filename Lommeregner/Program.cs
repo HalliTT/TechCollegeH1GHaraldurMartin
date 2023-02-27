ecd9e62e6b484a5b88364097c003fccedb566449
Haraldur <haraldur@live.dk>
2023-02-27 11:05:03 +0100

    Basic calc minus and multi

--- Lommeregner/Program.cs
+++ Lommeregner/Program.cs
@@ -6,7 +6,49 @@ namespace Lommeregner
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press 1 to minus or 2 to multiple ");
+            string menu = Console.ReadLine();
+
+            switch (menu)
+            {
+                case "1":
+                    Console.WriteLine("MINUS");
+                    Console.WriteLine("Write first number");
+                    string numberMinusOne = Console.ReadLine();
+                    Console.WriteLine("Write second number");
+                    string numberMinusTwo = Console.ReadLine();
+
+                    //Transform string to int
+                    int numberMinusOneInt = Int32.Parse(numberMinusOne);
+                    int numberMinusTwoInt = Int32.Parse(numberMinusTwo);
+
+                    //calculation
+                    int minus = numberMinusOneInt - numberMinusTwoInt;
+
+                    //Write out reulst
+                    Console.WriteLine(numberMinusOneInt + "-" + numberMinusTwoInt + " = " + minus);
+                    break;
+                case "2":
+                    Console.WriteLine("MULTIPLE");
+                    Console.WriteLine("Write first number");
+                    string numberMultiOne = Console.ReadLine();
+                    Console.WriteLine("Write second number");
+                    string numberMultiTwo = Console.ReadLine();
+                    
+                    //Transform string to int
+                    int numberMultiOneInt = Int32.Parse(numberMultiOne);
+                    int numberMultiTwoInt = Int32.Parse(numberMultiTwo);
+
+                    //calculation
+                    int multi = numberMultiOneInt * numberMultiTwoInt;
+
+                    //Write out reulst
+                    Console.WriteLine(numberMultiOneInt + "*" + numberMultiTwoInt + " = " + multi);
+                    break;
+                default:
+                    Console.WriteLine("Sorry try agian");
+                    break;
+            }
         }
     }
 }
