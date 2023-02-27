f05c2e8b58f45535e61d3fba4959aa69c238aa88
Miudmon <martbkmobil@gmail.com>
2023-02-27 11:05:00 +0100

    plus + dividere
    basicaf.

--- Lommeregner/Program.cs
+++ Lommeregner/Program.cs
@@ -4,9 +4,18 @@ namespace Lommeregner
 {
     class Program
     {
+        double tal1;
+        double tal2;
+        double resultat;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Tal 1");
+            double tal1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("tal 2");
+            double tal2 = Convert.ToDouble(Console.ReadLine());
+            double resultat = tal1 + tal2;
+            Console.Writeline("resultat er");
+            Console.WriteLine(resultat);
         }
     }
 }
